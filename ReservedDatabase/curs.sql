2dfa7dc55805e9e136ad310be1485a6bcaf75338
Ken7net <kornilov-max@yandex.ru>
2022-05-08 13:15:12 +0300

    Update curs.sql

--- ReservedDatabase/curs.sql
+++ ReservedDatabase/curs.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80028
  File Encoding         : 65001
 
- Date: 07/05/2022 10:32:42
+ Date: 08/05/2022 12:29:46
 */
 
 SET NAMES utf8mb4;
@@ -23,11 +23,10 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `company`;
 CREATE TABLE `company`  (
   `company_id` int NOT NULL AUTO_INCREMENT,
-  `company_name` varchar(15) CHARACTER SET cp1251 COLLATE cp1251_general_ci NOT NULL,
+  `company_name` varchar(15) CHARACTER SET cp1251 COLLATE cp1251_general_ci NULL DEFAULT NULL,
   `activity` varchar(40) CHARACTER SET cp1251 COLLATE cp1251_general_ci NULL DEFAULT NULL,
   `finance` int NULL DEFAULT 0,
-  PRIMARY KEY (`company_id`) USING BTREE,
-  UNIQUE INDEX `company_name`(`company_name`) USING BTREE
+  PRIMARY KEY (`company_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = cp1251 COLLATE = cp1251_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -37,6 +36,7 @@ INSERT INTO `company` VALUES (1, 'ОкнаСиль', 'Окна', 1590654);
 INSERT INTO `company` VALUES (2, 'Стиляги', 'Одежда', 15906);
 INSERT INTO `company` VALUES (3, 'IT Prog', 'IT', 1590654);
 INSERT INTO `company` VALUES (4, 'СООО\"Дивет\"', 'Мебель', 15902630);
+INSERT INTO `company` VALUES (5, 'Рога_и_копыта', 'Все_по_немногу', 15000000);
 
 -- ----------------------------
 -- Table structure for inventory
@@ -48,7 +48,7 @@ CREATE TABLE `inventory`  (
   `quantity` int NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `id`(`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = cp1251 COLLATE = cp1251_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = cp1251 COLLATE = cp1251_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of inventory
@@ -68,14 +68,14 @@ CREATE TABLE `mark`  (
   `project2_id` int NULL DEFAULT NULL,
   `mark1` float NULL DEFAULT NULL,
   `mark2` float NULL DEFAULT NULL,
-  INDEX `users_id`(`user_id`) USING BTREE,
-  INDEX `project1_id`(`project1_id`) USING BTREE,
-  INDEX `project2_id`(`project2_id`) USING BTREE,
   UNIQUE INDEX `mark_id`(`mark_id`) USING BTREE,
-  CONSTRAINT `mark_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `mark_ibfk_2` FOREIGN KEY (`project1_id`) REFERENCES `project` (`project_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `mark_ibfk_3` FOREIGN KEY (`project2_id`) REFERENCES `project` (`project_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = cp1251 COLLATE = cp1251_general_ci ROW_FORMAT = Dynamic;
+  INDEX `mark_ibfk_1`(`user_id`) USING BTREE,
+  INDEX `mark_ibfk_2`(`project1_id`) USING BTREE,
+  INDEX `mark_ibfk_3`(`project2_id`) USING BTREE,
+  CONSTRAINT `mark_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE SET NULL ON UPDATE SET NULL,
+  CONSTRAINT `mark_ibfk_2` FOREIGN KEY (`project1_id`) REFERENCES `project` (`project_id`) ON DELETE SET NULL ON UPDATE SET NULL,
+  CONSTRAINT `mark_ibfk_3` FOREIGN KEY (`project2_id`) REFERENCES `project` (`project_id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = cp1251 COLLATE = cp1251_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of mark
@@ -91,24 +91,24 @@ DROP TABLE IF EXISTS `project`;
 CREATE TABLE `project`  (
   `project_id` int NOT NULL AUTO_INCREMENT,
   `project_name` varchar(50) CHARACTER SET cp1251 COLLATE cp1251_general_ci NOT NULL,
-  `sum_credit` decimal(12, 3) NOT NULL,
+  `sum_credit` int NOT NULL,
   `credit_time` int NOT NULL,
   `sud_reestr` varchar(3) CHARACTER SET cp1251 COLLATE cp1251_general_ci NOT NULL DEFAULT 'Нет',
   `application_date` date NOT NULL,
   `company_id` int NULL DEFAULT NULL,
   PRIMARY KEY (`project_id`) USING BTREE,
-  INDEX `Project_fk1`(`company_id`) USING BTREE,
-  CONSTRAINT `Project_fk1` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = cp1251 COLLATE = cp1251_general_ci ROW_FORMAT = Dynamic;
+  INDEX `Project_fk1`(`company_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = cp1251 COLLATE = cp1251_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of project
 -- ----------------------------
-INSERT INTO `project` VALUES (1, 'ОКна+', 15559.000, 15, 'Нет', '2022-05-01', 1);
-INSERT INTO `project` VALUES (2, 'Новые ткани', 155.000, 2, 'Да', '2022-05-03', 2);
-INSERT INTO `project` VALUES (3, 'Новое оборудование', 46698.000, 5, 'Да', '2022-05-03', 3);
-INSERT INTO `project` VALUES (4, 'Новые_станки', 500000.000, 20, 'нет', '2022-05-06', 4);
-INSERT INTO `project` VALUES (5, 'Новый_магазин', 150000.000, 20, 'Нет', '2022-05-06', 1);
+INSERT INTO `project` VALUES (1, 'Окна++', 15559, 15, 'Нет', '2022-05-07', 1);
+INSERT INTO `project` VALUES (2, 'Новые ткани', 155, 2, 'Да', '2022-05-03', 2);
+INSERT INTO `project` VALUES (3, 'Новое оборудование', 46698, 5, 'Да', '2022-05-03', 3);
+INSERT INTO `project` VALUES (5, 'Новые_станки', 100500, 18, 'Нет', '2022-05-07', 1);
+INSERT INTO `project` VALUES (7, 'Модернизация_производства', 1500000, 20, 'Нет', '2022-05-07', 2);
+INSERT INTO `project` VALUES (8, 'Новый_магазин', 100500, 19, 'Нет', '2022-05-07', 1);
 
 -- ----------------------------
 -- Table structure for user
@@ -124,8 +124,9 @@ CREATE TABLE `user`  (
   UNIQUE INDEX `login`(`login`) USING BTREE,
   INDEX `role`(`role`) USING BTREE,
   INDEX `pass`(`pass`) USING BTREE,
-  INDEX `user_name`(`user_name`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = cp1251 COLLATE = cp1251_general_ci ROW_FORMAT = Dynamic;
+  INDEX `user_name`(`user_name`) USING BTREE,
+  CONSTRAINT `role_fk` FOREIGN KEY (`role`) REFERENCES `user_role` (`role`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = cp1251 COLLATE = cp1251_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
@@ -136,15 +137,17 @@ INSERT INTO `user` VALUES (3, 'Петр I', 'Petya', '183583', 'Эксперт')
 INSERT INTO `user` VALUES (8, 'Chapay Vasya', 'Chaplin', '1945', 'Администратор');
 INSERT INTO `user` VALUES (11, 'Чапаев Василий', 'Chaply', '1917', 'Эксперт');
 INSERT INTO `user` VALUES (12, 'Жуков Георгий', 'Zhuchara', '1945', 'Эксперт');
+INSERT INTO `user` VALUES (16, 'Выпивоха', 'DrinkingBoss', '123', 'Администратор');
+INSERT INTO `user` VALUES (17, 'Пьянь Подзаборная', 'Drink4ever', '654', 'Эксперт');
 
 -- ----------------------------
 -- Table structure for user_role
 -- ----------------------------
 DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role`  (
-  `name` varchar(40) CHARACTER SET cp1251 COLLATE cp1251_general_ci NOT NULL,
-  PRIMARY KEY (`name`) USING BTREE,
-  UNIQUE INDEX `name`(`name`) USING BTREE
+  `role` varchar(40) CHARACTER SET cp1251 COLLATE cp1251_general_ci NOT NULL,
+  PRIMARY KEY (`role`) USING BTREE,
+  UNIQUE INDEX `name`(`role`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = cp1251 COLLATE = cp1251_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
